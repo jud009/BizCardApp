01999de3c188d620011985715c3be4a1eea4396e
udj009 <judiclei41@gmail.com>
2021-12-10 12:14:30 -0300

    creating card

--- app/src/main/java/com/udj/bizcard/MainActivity.kt
+++ app/src/main/java/com/udj/bizcard/MainActivity.kt
@@ -3,11 +3,17 @@ package com.udj.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.udj.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +23,7 @@ class MainActivity : ComponentActivity() {
             BizCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    createBiCard()
                 }
             }
         }
@@ -25,14 +31,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun createBiCard() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier.height(300.dp).width(300.dp).padding(16.dp),
+            elevation = 4.dp,
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            backgroundColor = Color.White
+        ) {
+            
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BizCardTheme {
-        Greeting("Android")
+        createBiCard()
     }
 }
