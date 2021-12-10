953e214dec7969208973ce14799c33af627e9c23
udj009 <judiclei41@gmail.com>
2021-12-10 13:38:08 -0300

    adding image

--- app/src/main/java/com/udj/bizcard/MainActivity.kt
+++ app/src/main/java/com/udj/bizcard/MainActivity.kt
@@ -3,7 +3,10 @@ package com.udj.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,6 +15,9 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.udj.bizcard.ui.theme.BizCardTheme
@@ -37,12 +43,29 @@ fun createBiCard() {
         .fillMaxHeight()
     ) {
         Card(
-            modifier = Modifier.height(300.dp).width(300.dp).padding(16.dp),
+            modifier = Modifier
+                .height(300.dp)
+                .width(300.dp)
+                .padding(16.dp),
             elevation = 4.dp,
             shape = RoundedCornerShape(corner = CornerSize(16.dp)),
             backgroundColor = Color.White
         ) {
-            
+            Surface(
+                modifier = Modifier
+                    .size(128.dp)
+                    .padding(8.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+            ) {
+                Image(painter = painterResource(id = R.drawable.user_profile),
+                    contentDescription = stringResource(R.string.user_profile_image),
+                    modifier = Modifier.size(124.dp),
+                    contentScale = ContentScale.Crop
+                    )
+            }
         }
     }
 }
